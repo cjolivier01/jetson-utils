9a76a6688b769feeaa2f0b0e529db4104906a3ec
Christopher Olivier <colivier@tesla.com>
2025-02-03 12:16:41 -0800

    ..

--- cuda/testCudaBlend.cpp
+++ cuda/testCudaBlend.cpp
@@ -119,7 +119,7 @@ int main(int argc, char** argv) {
   auto start_ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
                 .count();
   
-  size_t frame_count = 100;
+  size_t frame_count = 1000;
   for (size_t i = 0; i < frame_count; ++i) {
     cudaLaplacianBlendWithContext(
         (const float*)cudaImage1Float.data(),
