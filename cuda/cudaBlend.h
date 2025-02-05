de9597ee1eb6aa62e6112b81c4aafe9189c54229
Chris Olivier <cjolivier01@apache.org>
2025-02-05 10:52:29 -0800

    ..

--- cuda/cudaBlend.h
+++ cuda/cudaBlend.h
@@ -34,14 +34,14 @@ struct CudaBatchLaplacianBlendContext {
 
   ~CudaBatchLaplacianBlendContext() {
     for (int level = 0; level < numLevels; level++) {
-      maybeCudaFree(d_gauss1[level]);
-      maybeCudaFree(d_gauss2[level]);
       maybeCudaFree(d_lap1[level]);
       maybeCudaFree(d_lap2[level]);
       maybeCudaFree(d_blend[level]);
       maybeCudaFree(d_resonstruct[level]);
       if (level) {
         // Level 0 is owned by user code (passed into the function each time)
+        maybeCudaFree(d_gauss1[level]);
+        maybeCudaFree(d_gauss2[level]);
         maybeCudaFree(d_maskPyr[level]);
       }
     }
