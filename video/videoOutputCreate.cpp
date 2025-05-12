65eed6b6b9b01b26ee458562ba529b2dd2a24b61
Chris Olivier <cjolivier01@apache.org>
2025-05-11 17:34:46 -0700

    ..

--- video/videoOutputCreate.cpp
+++ video/videoOutputCreate.cpp
@@ -123,7 +123,7 @@ videoOutput* videoOutput::Create( const char* resource, const commandLine& cmdLi
 	
 	if( !output )
 	{
-		if( positionArg >= cmdLine.GetPositionArgs() && (resource == NULL || strlen(resource) == 0) )
+		if( positionArg >= (int)cmdLine.GetPositionArgs() && (resource == NULL || strlen(resource) == 0) )
 			return CreateNullOutput();  // only create a fake sink when the output was unspecified
 		else
 			return NULL;
