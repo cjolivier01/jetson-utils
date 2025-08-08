e4c24fa62558d1029ffb1cfc119b5ed5e28186c2
Christopher Olivier <cjolivier01@gmail.com>
2025-08-08 09:40:02 -0700

    ..

--- core/force_refs.cpp
+++ core/force_refs.cpp
@@ -10,10 +10,8 @@
 extern "C" void _force_link_all_symbols() {
     videoOptions opts;
     const char* uri = "";
-    const char* argv[] = {nullptr};
-    int argc = 0;
-    videoSource::Create(uri, argc, (char**)argv, opts);
-    videoOutput::Create(uri, argc, (char**)argv, opts);
+    videoSource::Create(uri, opts);
+    videoOutput::Create(uri, opts);
 
     void* p = nullptr;
     const float4 color = {0,0,0,0};
@@ -21,4 +19,3 @@ extern "C" void _force_link_all_symbols() {
     (void)cudaResize(p, 0, 0, p, 0, 0, IMAGE_UNKNOWN, FILTER_POINT, 0);
     (void)cudaDrawLine(p, p, 0, 0, IMAGE_UNKNOWN, 0, 0, 0, 0, color, 1.0f, 0);
 }
-
